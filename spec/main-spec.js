8b241d61df32c4b8422beb02bd37e6a94c2738b5
e <1456958517@qq.com>
2017-07-26 14:33:29 +0800

    first

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,7 +2,7 @@ const add = require('../main/main');
 
 describe('main()', () => {
 
-    it('should return added result', () => {
+    it('should return added result',() => {
         let actual = add(1,1);
         expect(actual).toBe(2);
     });
